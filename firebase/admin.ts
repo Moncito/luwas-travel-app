2fd10dd0f08621e79215b9ecb9c66359e8cdc70f
Moncito <moncitoglenn03@gmail.com>
2025-06-02 01:15:18 +0800

    fix: Firebase Admin private key env setup

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
 
 if (!getApps().length) {
+  const serviceAccount = JSON.parse(
+    Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_KEY!, 'base64').toString('utf8')
+  );
+
   initializeApp({
-    credential: cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-    }),
+    credential: cert(serviceAccount),
   });
 }
 
